f54ac26168eeec442f159a68b20a78053387ba9c
eddtries <eddtries@icloud.com>
2023-10-26 08:54:56 +0200

    Add prompts dir for DBML to SQL

---
+++ prompts/sql/create_tables.prompt.md
@@ -0,0 +1,18 @@
+Convert the following DBML to valid PostgreSQL, following all rules.
+
+Rules: 
+1. All tables must be CREATE TABLE IF NOT EXISTS.
+2. All primary keys must be auto-incrementing.
+3. Only include valid postgresql code in your answer, do not describe anything in text.
+
+```
+Table fighter {
+  id integer [primary key]
+  name string
+}
+
+Table fight {
+  fighter_id_b integer [ref: - fighter.id]
+  fighter_id_a integer [ref: - fighter.id]
+}
+```
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,10 +1,14 @@
+-- Create fighter table if it doesn't exist
 CREATE TABLE IF NOT EXISTS fighter (
-  fighter_id INT NOT NULL,
-  name varchar(100) NOT NULL,
-  PRIMARY KEY (fighter_id)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
 );
 
+-- Create fight table if it doesn't exist
 CREATE TABLE IF NOT EXISTS fight (
-  fight_id INT NOT NULL,
-  PRIMARY KEY (fight_id)
+    id SERIAL PRIMARY KEY,
+    fighter_id_b INTEGER,
+    fighter_id_a INTEGER,
+    FOREIGN KEY (fighter_id_b) REFERENCES fighter(id),
+    FOREIGN KEY (fighter_id_a) REFERENCES fighter(id)
 );
